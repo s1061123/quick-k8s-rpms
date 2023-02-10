0411224067d8991030a0171c88ba7ec234bfeeaf
Tomofumi Hayashi <tohayash@redhat.com>
2023-02-11 02:33:43 +0900

    mod

--- k8s-default-image-push.sh
+++ k8s-default-image-push.sh
@@ -4,14 +4,23 @@ TOP=$(dirname $(readlink -f $0))
 . ${TOP}/common.sh
 
 # copy & push required container image
-docker pull registry.k8s.io/coredns/coredns:v1.9.3
-docker tag registry.k8s.io/coredns/coredns:v1.9.3 $REPO/coredns:v1.9.3
-docker push $REPO/coredns:v1.9.3
+# You may need to update rersion
 
-docker pull registry.k8s.io/etcd:3.5.6-0
-docker tag registry.k8s.io/etcd:3.5.6-0 $REPO/etcd:3.5.6-0
-docker push $REPO/etcd:3.5.6-0
+# CoreDNSVersion in ./cmd/kubeadm/app/constants/constants.go
+coredns_ver="v1.10.0"
+# DefaultEtcdVersion in ./cmd/kubeadm/app/constants/constants.go
+etcd_ver="3.5.7-0"
+# PauseVersion in ./cmd/kubeadm/app/constants/constants.go
+pause_ver="3.9"
 
-docker pull registry.k8s.io/pause:3.9
-docker tag registry.k8s.io/pause:3.9 $REPO/pause:3.9
-docker push $REPO/pause:3.9
+docker pull registry.k8s.io/coredns/coredns:${coredns_ver}
+docker tag registry.k8s.io/coredns/coredns:${coredns_ver} ${REPO}/coredns:${coredns_ver}
+docker push ${REPO}/coredns:${coredns_ver}
+
+docker pull registry.k8s.io/etcd:${etcd_ver}
+docker tag registry.k8s.io/etcd:${etcd_ver} ${REPO}/etcd:${etcd_ver}
+docker push ${REPO}/etcd:${etcd_ver}
+
+docker pull registry.k8s.io/pause:${pause_ver}
+docker tag registry.k8s.io/pause:${pause_ver} ${REPO}/pause:${pause_ver}
+docker push ${REPO}/pause:${pause_ver}
